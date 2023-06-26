242f4892e5e9c79fe6cbe204277d52830806d01b
Cales <86434489+calesj@users.noreply.github.com>
2023-06-26 14:27:06 -0300

    feat - adicionando o ModeratorController.php e extendendo a classe ControllerBase

--- server/app/Http/Controllers/ModeratorController.php
+++ server/app/Http/Controllers/ModeratorController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Moderator;
 use Illuminate\Http\Request;
 
-class ModeratorController extends Controller
+class ModeratorController extends ControllerBase
 {
-    //
+    public function __construct()
+    {
+        $this->model = Moderator::class;
+        $this->user_level = 'moderator';
+    }
 }
