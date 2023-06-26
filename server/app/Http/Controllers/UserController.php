b6c31f8e89f975ee60e2b4079afba9bedb214516
Cales <86434489+calesj@users.noreply.github.com>
2023-06-26 14:26:33 -0300

    feat - adicionando o UserController, para registro de usuario, e login

--- server/app/Http/Controllers/UserController.php
+++ server/app/Http/Controllers/UserController.php
@@ -2,32 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Form\FormValidation;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use phpseclib3\Crypt\Hash;
 
 class UserController extends Controller
 {
+    /** REGRAS DE VALIDACAO */
+    private $rulesRegister = [
+        'name' => 'required',
+        'email' => 'required|unique:users',
+        'password' => 'required|min:3',
+        'user_level' => 'required'
+    ];
+
+    private $rulesLogin = [
+        'email' => 'required',
+        'password' => 'required'
+    ];
+
+    /**
+     * METODO DE LOGIN
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function login(Request $request)
     {
-        $credentials = [
-            'email' => $request->email,
-            'password' => bcrypt($request->password)
-        ];
+        $validate = FormValidation::validate($request->only(['email', 'password']), $this->rulesLogin);
 
-        if(!auth('web')->attempt($credentials)) {
-            return response()->json([
-                'success' => false,
-                'message' => 'email or password is incorrect',
-                'user' => null
-            ]);
+        if($validate !== true) {
+            return $validate;
         }
 
-        return response()->json([
-           'success' => true,
-           'message' => 'success',
-           'user' => Auth::user()
-        ]);
+        // CASO NAO PASSE NA AUTENTICACAO
+        $invalidData = response()->json(['errors' => ['login' => 'dados invalidos']]);
+
+        $user = User::where('email', $request->email)->first();
+
+        // SE EXISITR UM USUARIO COM O E-MAIL ENVIADO
+        if($user) {
+            if (\Illuminate\Support\Facades\Hash::check($request->password, $user->password)) {
+                $token = $user->createToken('access_token')->accessToken;
+                return response()->json(['access_token' => $token]);
+            }
+            return $invalidData;
+        }
+
+        return $invalidData;
     }
 
     /**
@@ -37,13 +60,29 @@ class UserController extends Controller
      */
     public function register(Request $request)
     {
-        $password = bcrypt($request->password);
-        $data = User::create($request->only('name', 'email', $password));
+        $validate = FormValidation::validate($request->only(['name', 'email', 'password']), $this->rulesRegister);
 
-        return response()->json([
-            'success' => true,
-            'message' => 'success',
-            'data' => $data
-        ],200);
+        if($validate !== true) {
+            return $validate;
+        }
+        $name = $request->name;
+        $email = $request->email;
+        $userLevel = $request->user_level;
+        $password = bcrypt($request->password);
+
+        $user = new User();
+        $user->name = $name;
+        $user->email = $email;
+        $user->user_level = $userLevel;
+        $user->password = $password;
+        $user->save();
+
+        if($user) {
+            return response()->json([
+                'success' => true,
+                'message' => 'success',
+                'data' => $user
+            ],200);
+        }
     }
 }
