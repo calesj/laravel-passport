39cb7c9f36a4231461b0bafd1abd9f839d194ebb
Cales <86434489+calesj@users.noreply.github.com>
2023-06-26 14:28:17 -0300

    feat - adicionando UserPolicy, politica de quem pode ou nao pode executar alguma coisa

---
+++ server/app/Policies/UserPolicy.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class UserPolicy
+{
+    /**
+     * QUE PODEM VISUALIZAR OS REGISTROS
+     */
+    public function viewAny(User $user): bool
+    {
+        if ($user->user_level === 'administrator' || $user->user_level === 'financial' || $user->user_level === 'financial_2' || $user->user_level === 'moderator') {
+            return true;
+        }
+
+        return false;
+    }
+    /**
+     * QUEM PODE CRIAR UM REGISTRO
+     */
+    public function create(User $user): bool
+    {
+        if ($user->user_level === 'administrator' || $user->user_level === 'financial' || $user->user_level === 'financial_2') {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * QUEM PODE ATUALIZAR UM REGISTRO
+     */
+    public function update(User $user, User $model): bool
+    {
+        if ($user->user_level === 'administrator' || $user->user_level === 'financial' || $user->user_level === 'financial_2') {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * QUEM PODE EXCLUIR UM REGISTRO
+     */
+    public function delete(User $user, User $model): bool
+    {
+        if ($user->user_level === 'administrator' || $user->user_level === 'financial') {
+            return true;
+        }
+
+        return false;
+    }
+}
