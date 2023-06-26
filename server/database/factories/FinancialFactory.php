5f5e7587b3b066006fedd3c504750d957bce78ec
Cales <86434489+calesj@users.noreply.github.com>
2023-06-26 15:11:16 -0300

    feat - criando a factory da tabela financials

---
+++ server/database/factories/FinancialFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Financial;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Financial>
+ */
+class FinancialFactory extends Factory
+{
+    protected $model = Financial::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory()->create(['user_level' => 'financial',])->id
+        ];
+    }
+}
