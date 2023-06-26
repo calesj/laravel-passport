49feeb8dab40e532e330a9ad09a6ebe352e63753
Cales <86434489+calesj@users.noreply.github.com>
2023-06-26 14:25:10 -0300

    feat - adicionando alguns dados padroes para testar o funcionamento do projeto

--- server/database/seeders/DatabaseSeeder.php
+++ server/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Administrator;
+use App\Models\Financial;
+use App\Models\Moderator;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,10 +17,46 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-         \App\Models\User::factory()->create([
+        /**
+         * USUARIO ADMINISTRADOR
+         */
+         $userAdmin = \App\Models\User::factory()->create([
              'name' => 'admin',
-             'email' => 'admin@admin.com',
-             'password' => bcrypt('12345')
+             'email' => 'admin@email.com',
+             'password' => bcrypt('12345'),
+             'user_level' => 'administrator'
          ]);
+
+         $administrator = new Administrator();
+         $administrator->user_id = $userAdmin->id;
+         $administrator->save();
+
+        /**
+         * USUARIO MODERADOR
+         */
+        $userMod = \App\Models\User::factory()->create([
+            'name' => 'mod',
+            'email' => 'mod@email.com',
+            'password' => bcrypt('12345'),
+            'user_level' => 'moderator'
+        ]);
+
+        $moderator = new Moderator();
+        $moderator->user_id = $userMod->id;
+        $moderator->save();
+
+        /**
+         * USUARIO FINANCEIRO
+         */
+        $userFinancial = \App\Models\User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('12345'),
+            'user_level' => 'financial'
+        ]);
+
+        $financial = new Financial();
+        $financial->user_id = $userFinancial->id;
+        $financial->save();
     }
 }
