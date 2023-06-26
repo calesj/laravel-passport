f0009fc20dbb55437009ca16af205188b567cfd4
Cales <86434489+calesj@users.noreply.github.com>
2023-06-26 14:24:47 -0300

    feat - registrando `policies`, configurando algumas coisas do `passport`, e criando alguns gates

--- server/app/Providers/AuthServiceProvider.php
+++ server/app/Providers/AuthServiceProvider.php
@@ -3,7 +3,11 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Models\User;
+use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +25,16 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
+
+        Passport::tokensExpireIn(now()->addDays(15));
+        Passport::refreshTokensExpireIn(now()->addDays(30));
+        Passport::personalAccessTokensExpireIn(now()->addMonths(6));
+
+        // GATES DE NIVEL DE ACESSO
+        Gate::define('view-user', [UserPolicy::class, 'viewAny']);
+        Gate::define('create-user', [UserPolicy::class, 'create']);
+        Gate::define('update-user', [UserPolicy::class, 'update']);
+        Gate::define('delete-user', [UserPolicy::class, 'delete']);
     }
 }
