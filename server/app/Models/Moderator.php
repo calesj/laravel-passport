b9b75c3303a01e599850d6d3ba297c2a82e83750
Cales <86434489+calesj@users.noreply.github.com>
2023-06-26 14:25:33 -0300

    feat - adicionando o model Moderator, e o relacionamento com a tabela users

--- server/app/Models/Moderator.php
+++ server/app/Models/Moderator.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Moderator extends Model
 {
     use HasFactory;
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
