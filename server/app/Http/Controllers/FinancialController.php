215e8db81cdf8bf2842ab46f251bbcb431a5b1ab
Cales <86434489+calesj@users.noreply.github.com>
2023-06-26 14:27:00 -0300

    feat - adicionando o FinancialController.php e extendendo a classe ControllerBase

--- server/app/Http/Controllers/FinancialController.php
+++ server/app/Http/Controllers/FinancialController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Financial;
 use Illuminate\Http\Request;
 
-class FinancialController extends Controller
+class FinancialController extends ControllerBase
 {
-    //
+    public function __construct()
+    {
+        $this->model = Financial::class;
+        $this->user_level = 'financial';
+    }
 }
