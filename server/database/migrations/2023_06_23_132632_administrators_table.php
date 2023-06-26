fac8d568ec4378da4e124eda66c06305c29fc0bc
Cales <86434489+calesj@users.noreply.github.com>
2023-06-26 14:23:53 -0300

    feat - criando as migrations necessarias para o nosso projeto

--- server/database/migrations/2023_06_23_132632_administrators_table.php
+++ server/database/migrations/2023_06_23_132632_administrators_table.php
@@ -11,7 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('administrators', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('administrators');
     }
 };
