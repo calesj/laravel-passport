1d80faff84a7e3615b929b348729b8b2d6fa890a
Cales <86434489+calesj@users.noreply.github.com>
2023-06-26 14:27:36 -0300

    feat - adicionando as rotas de CRUD, para as tabelas

--- server/routes/api.php
+++ server/routes/api.php
@@ -22,6 +22,32 @@ Route::post('register', [\App\Http\Controllers\UserController::class, 'register'
 
 Route::post('login', [\App\Http\Controllers\UserController::class, 'login']);
 
+// ADMINISTRADOR
+Route::prefix('/administrator')->group(function () {
+    Route::get('', [\App\Http\Controllers\AdministratorController::class, 'index'])->middleware('can:view-user');
+    Route::get('/{id}', [\App\Http\Controllers\AdministratorController::class, 'show'])->middleware('can:view-user');
+    Route::post('', [\App\Http\Controllers\AdministratorController::class, 'create'])->middleware('can:create-user');
+    Route::post('/{id}', [\App\Http\Controllers\AdministratorController::class, 'update'])->middleware('can:update-user');
+    Route::delete('', [\App\Http\Controllers\AdministratorController::class, 'delete'])->middleware('can:delete-user');
+})->middleware('auth:api');
+
+// MODERADOR
+Route::prefix('/moderator')->group(function () {
+    Route::get('', [\App\Http\Controllers\ModeratorController::class, 'index'])->middleware('can:view-user');
+    Route::get('/{id}', [\App\Http\Controllers\ModeratorController::class, 'show'])->middleware('can:view-user');
+    Route::post('', [\App\Http\Controllers\ModeratorController::class, 'create'])->middleware('can:create-user');
+    Route::post('/{id}', [\App\Http\Controllers\ModeratorController::class, 'update'])->middleware('can:update-user');
+    Route::delete('', [\App\Http\Controllers\ModeratorController::class, 'delete'])->middleware('can:delete-user');
+})->middleware('auth:api');
+
+Route::prefix('/financial')->group(function () {
+    Route::get('', [\App\Http\Controllers\FinancialController::class, 'index'])->middleware('can:view-user');
+    Route::get('/{id}', [\App\Http\Controllers\FinancialController::class, 'show'])->middleware('can:view-user');
+    Route::post('', [\App\Http\Controllers\FinancialController::class, 'create'])->middleware('can:create-user');
+    Route::post('/{id}', [\App\Http\Controllers\FinancialController::class, 'update'])->middleware('can:update-user');
+    Route::delete('', [\App\Http\Controllers\FinancialController::class, 'delete'])->middleware('can:delete-user');
+})->middleware('auth:api');
+
 
 
 
