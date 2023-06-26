04ff20c98f8d5a6f6f65dde2b4d31b6490198c66
Cales <86434489+calesj@users.noreply.github.com>
2023-06-26 14:24:08 -0300

    feat - criando o AdministratorController.php

--- server/app/Http/Controllers/AdministratorController.php
+++ server/app/Http/Controllers/AdministratorController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Form\FormValidation;
+use App\Models\Administrator;
+use App\Models\User;
 use Illuminate\Http\Request;
 
-class AdministratorController extends Controller
+class AdministratorController extends ControllerBase
 {
-    //
+    public function __construct()
+    {
+        $this->model = Administrator::class;
+        $this->user_level = 'administrator';
+    }
 }
